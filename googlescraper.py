9b528b19cdb6869135992e259d3ecb5f6a98af40
Vaishnav <methylalcohol420@gmail.com>
2018-09-07 18:25:21 +0530

    Corrected the code

--- googlescraper.py
+++ googlescraper.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(executable_path="/Users/shiva/Desktop/chromedriver.exe")
+driver = webdriver.Chrome(executable_path="/Users/chromedriver.exe")
 
 driver.get("https://www.google.in")
 driver.find_element_by_xpath("""//*[@id="lst-ib"]""").send_keys("FOSS@Amrita\ue007")
